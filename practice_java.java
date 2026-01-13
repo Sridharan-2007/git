6b07d07bc0286d1210d2975da6d1edd91977cd01
Sridharan-2007 <dharansri449@gmail.com>
2026-01-13 12:24:17 +0530

    over

---
+++ practice_java.java
@@ -0,0 +1,9 @@
+public class practice_java {
+    public static void main(String[] args) {
+       int a,b;
+       a=10;
+       b=20;
+       System.out.println("The sum of a and b is: " + (a + b
+
+    }
+}
---
+++ practice_js.js
@@ -0,0 +1,17 @@
+// 1. Declare variables
+const name = "Developer";
+const year = 2026;
+
+// 2. Create a function
+function greet(userName) {
+    return "Hello, " + userName + "! Welcome to JavaScript.";
+}
+
+// 3. Print output to the console
+console.log(greet(name));
+console.log("The current year is: " + year);
+
+// 4. A simple calculation
+let a = 10;
+let b = 5;
+console.log("Sum of " + a + " + " + b + " is: " + (a + b));
